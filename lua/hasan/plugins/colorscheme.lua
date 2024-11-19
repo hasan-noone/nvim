273c0978d210ef5248567c842762ec1318cfe8fd
MD NAUMAN ATHAR HASAN <mdnaumanatharhasan@MDs-MacBook-Air.local>
2024-11-20 00:02:50 +0530

    changed some keymaps and changed colorscheme

--- lua/hasan/plugins/colorscheme.lua
+++ lua/hasan/plugins/colorscheme.lua
@@ -1,33 +1,33 @@
 return {
 	"rebelot/kanagawa.nvim",
 	config = function()
-		-- Default options:
 		require("kanagawa").setup({
-			compile = false, -- enable compiling the colorscheme
-			undercurl = true, -- enable undercurls
+			compile = true,
+			undercurl = true,
 			commentStyle = { italic = true },
 			functionStyle = {},
 			keywordStyle = { italic = true },
 			statementStyle = { bold = true },
 			typeStyle = {},
-			transparent = true, -- do not set background color
-			dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-			terminalColors = true, -- define vim.g.terminal_color_{0,17}
-			colors = { -- add/modify theme and palette colors
+			transparent = false,
+			dimInactive = false,
+			terminalColors = true,
+			colors = {
 				palette = {},
 				theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
 			},
-			overrides = function(colors) -- add/modify highlights
-				return {}
+			overrides = function(colors)
+				return {
+					CursorLine = { bg = colors.palette.sumiInk4 },
+					Comment = { fg = colors.palette.fujiGray, italic = true },
+				}
 			end,
-			theme = "wave", -- Load "wave" theme when 'background' option is not set
-			background = { -- map the value of 'background' option to a theme
-				dark = "wave", -- try "dragon" !
+			theme = "wave",
+			background = {
+				dark = "dragon",
 				light = "lotus",
 			},
 		})
-
-		-- setup must be called before loading
-		vim.cmd("colorscheme kanagawa")
+		vim.cmd("colorscheme kanagawa-wave")
 	end,
 }
