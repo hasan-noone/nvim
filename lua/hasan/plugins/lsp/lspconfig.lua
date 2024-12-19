bebd4882b36059e24a18095e9fa01c9d8976be53
MD NAUMAN ATHAR HASAN <mdnaumanatharhasan@MDs-MacBook-Air.local>
2024-12-19 22:26:57 +0530

    added some minor changes

--- lua/hasan/plugins/lsp/lspconfig.lua
+++ lua/hasan/plugins/lsp/lspconfig.lua
@@ -7,149 +7,69 @@ return {
 		{ "folke/neodev.nvim", opts = {} },
 	},
 	config = function()
-		-- import lspconfig plugin
+		-- Load necessary modules
 		local lspconfig = require("lspconfig")
-
-		-- import mason_lspconfig plugin
 		local mason_lspconfig = require("mason-lspconfig")
-
-		-- import cmp-nvim-lsp plugin
 		local cmp_nvim_lsp = require("cmp_nvim_lsp")
+		local keymap = vim.keymap
+		local capabilities = cmp_nvim_lsp.default_capabilities()
 
-		local keymap = vim.keymap -- for conciseness
+		-- Setup diagnostic signs
+		local signs = { Error = "", Warn = "", Hint = "󰠠", Info = "" }
+		for type, icon in pairs(signs) do
+			vim.fn.sign_define("DiagnosticSign" .. type, { text = icon, texthl = "DiagnosticSign" .. type, numhl = "" })
+		end
 
+		-- LSP Attach keybindings
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 			callback = function(ev)
-				-- Buffer local mappings.
-				-- See `:help vim.lsp.*` for documentation on any of the below functions
 				local opts = { buffer = ev.buf, silent = true }
 
-				-- set keybinds
-				opts.desc = "Show LSP references"
-				keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+				-- Setup common keymaps
+				local bindings = {
+					{ "gR", "<cmd>Telescope lsp_references<CR>", "Show LSP references" },
+					{ "gD", vim.lsp.buf.declaration, "Go to declaration" },
+					{ "gd", "<cmd>Telescope lsp_definitions<CR>", "Show LSP definitions" },
+					{ "gi", "<cmd>Telescope lsp_implementations<CR>", "Show LSP implementations" },
+					{ "gt", "<cmd>Telescope lsp_type_definitions<CR>", "Show LSP type definitions" },
+					{ "<leader>ca", vim.lsp.buf.code_action, "See available code actions" },
+					{ "<leader>rn", vim.lsp.buf.rename, "Smart rename" },
+					{ "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", "Show buffer diagnostics" },
+					{ "<leader>d", vim.diagnostic.open_float, "Show line diagnostics" },
+					{ "[d", vim.diagnostic.goto_prev, "Go to previous diagnostic" },
+					{ "]d", vim.diagnostic.goto_next, "Go to next diagnostic" },
+					{
+						"K",
+						vim.lsp.buf.hover,
+						"Show documentation for what is under cursor",
+					},
+					{ "<leader>rs", ":LspRestart<CR>", "Restart LSP" },
+				}
 
-				opts.desc = "Go to declaration"
-				keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
-
-				opts.desc = "Show LSP definitions"
-				keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
-
-				opts.desc = "Show LSP implementations"
-				keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
-
-				opts.desc = "Show LSP type definitions"
-				keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
-
-				opts.desc = "See available code actions"
-				keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
-
-				opts.desc = "Smart rename"
-				keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
-
-				opts.desc = "Show buffer diagnostics"
-				keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
-
-				opts.desc = "Show line diagnostics"
-				keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
-
-				opts.desc = "Go to previous diagnostic"
-				keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
-
-				opts.desc = "Go to next diagnostic"
-				keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
-
-				opts.desc = "Show documentation for what is under cursor"
-				keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
-
-				opts.desc = "Restart LSP"
-				keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+				-- Assign the keymaps
+				for _, binding in ipairs(bindings) do
+					opts.desc = binding[3]
+					keymap.set("n", binding[1], binding[2], opts)
+				end
 			end,
 		})
 
-		-- used to enable autocompletion (assign to every lsp server config)
-		local capabilities = cmp_nvim_lsp.default_capabilities()
-
-		-- Change the Diagnostic symbols in the sign column (gutter)
-		-- (not in youtube nvim video)
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		end
-
+		-- Setup mason LSP server handlers
 		mason_lspconfig.setup_handlers({
-			-- default handler for installed servers
 			function(server_name)
-				lspconfig[server_name].setup({
-					capabilities = capabilities,
-				})
+				lspconfig[server_name].setup({ capabilities = capabilities })
 			end,
-			["svelte"] = function()
-				-- configure svelte server
-				lspconfig["svelte"].setup({
-					capabilities = capabilities,
-					on_attach = function(client, bufnr)
-						vim.api.nvim_create_autocmd("BufWritePost", {
-							pattern = { "*.js", "*.ts" },
-							callback = function(ctx)
-								-- Here use ctx.match instead of ctx.file
-								client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
-							end,
-						})
-					end,
-				})
-			end,
-			["graphql"] = function()
-				-- configure graphql language server
-				lspconfig["graphql"].setup({
-					capabilities = capabilities,
-					filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
-				})
-			end,
-			["emmet_ls"] = function()
-				-- configure emmet language server
-				lspconfig["emmet_ls"].setup({
-					capabilities = capabilities,
-					filetypes = {
-						"html",
-						"typescriptreact",
-						"javascriptreact",
-						"css",
-						"sass",
-						"scss",
-						"less",
-						"svelte",
-						"cpp",
-						"python",
-					},
-				})
-			end,
+
+			-- Example of a server with custom setup
 			["lua_ls"] = function()
-				-- configure lua server (with special settings)
 				lspconfig["lua_ls"].setup({
 					capabilities = capabilities,
 					settings = {
 						Lua = {
-							-- make the language server recognize "vim" global
-							diagnostics = {
-								globals = { "vim" },
-							},
-							completion = {
-								callSnippet = "Replace",
-							},
+							diagnostics = { globals = { "vim" } },
+							completion = { callSnippet = "Replace" },
 						},
-					},
-				})
-			end,
-			["denols"] = function()
-				-- configure Deno language server
-				lspconfig["denols"].setup({
-					capabilities = capabilities,
-					root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
-					init_options = {
-						lint = true,
-						unstable = true,
 					},
 				})
 			end,
--- lua/hasan/plugins/lsp/mason.lua
+++ lua/hasan/plugins/lsp/mason.lua
@@ -5,15 +5,12 @@ return {
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
 	},
 	config = function()
-		-- import mason
+		-- Load necessary modules
 		local mason = require("mason")
-
-		-- import mason-lspconfig
 		local mason_lspconfig = require("mason-lspconfig")
-
 		local mason_tool_installer = require("mason-tool-installer")
 
-		-- enable mason and configure icons
+		-- Mason setup
 		mason.setup({
 			ui = {
 				icons = {
@@ -24,76 +21,65 @@ return {
 			},
 		})
 
+		-- LSP servers to install
+		local lsp_servers = {
+			"denols",
+			"ts_ls",
+			"html",
+			"cssls",
+			"tailwindcss",
+			"svelte",
+			"lua_ls",
+			"graphql",
+			"emmet_ls",
+			"prismals",
+			"pyright",
+			"clangd",
+		}
+
 		mason_lspconfig.setup({
-			-- list of servers for mason to install
-			ensure_installed = {
-				"denols",
-				"ts_ls",
-				"html",
-				"cssls",
-				"tailwindcss",
-				"svelte",
-				"lua_ls",
-				"graphql",
-				"emmet_ls",
-				"prismals",
-				"pyright",
-				"clangd",
-				"gopls",
-			},
+			ensure_installed = lsp_servers,
 		})
 
+		-- Tools to install
+		local tools = {
+			"prettier",
+			"stylua",
+			"isort",
+			"black",
+			"clang-format",
+		}
+
 		mason_tool_installer.setup({
-			ensure_installed = {
-				"prettier", -- prettier formatter
-				"stylua", -- lua formatter
-				"isort", -- python formatter
-				"black", -- python formatter
-				"clang-format", -- C/C++ formatter
-				"gofumpt", -- Go formatter
-			},
+			ensure_installed = tools,
 			automatic_installation = true,
 		})
 
-		-- Format on save and ensure tab width of 2 spaces
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = {
-				"javascript",
-				"typescript",
-				"html",
-				"css",
-				"json",
-				"yaml",
-				"markdown",
-				"graphql",
-				"liquid",
-				"lua",
-				"python",
-				"go",
-				"cpp",
-				"c",
-			},
-			callback = function()
-				-- Ensure tab size is set to 2 spaces
-				vim.bo.tabstop = 2
-				vim.bo.shiftwidth = 2
-				vim.bo.softtabstop = 2
-				vim.bo.expandtab = true -- Use spaces instead of tabs
+		-- Auto format and set tab width to 4 spaces
+		local format_files = {
+			"javascript",
+			"typescript",
+			"html",
+			"css",
+			"json",
+			"yaml",
+			"markdown",
+			"graphql",
+			"liquid",
+			"lua",
+			"python",
+			"cpp",
+			"c",
+		}
 
-				-- Format options for specific tools
-				if vim.bo.filetype == "javascript" or vim.bo.filetype == "typescript" then
-					vim.cmd(
-						[[autocmd FileType javascript,typescript setlocal tabstop=2 shiftwidth=2 softtabstop=2 expandtab]]
-					)
-				elseif vim.bo.filetype == "lua" then
-					vim.cmd([[autocmd FileType lua setlocal tabstop=2 shiftwidth=2 softtabstop=2 expandtab]])
-				elseif vim.bo.filetype == "python" then
-					vim.cmd([[autocmd FileType python setlocal tabstop=2 shiftwidth=2 softtabstop=2 expandtab]])
-				elseif vim.bo.filetype == "go" then
-					vim.cmd([[autocmd FileType go setlocal tabstop=2 shiftwidth=2 softtabstop=2 expandtab]])
-				elseif vim.bo.filetype == "cpp" or vim.bo.filetype == "c" then
-					vim.cmd([[autocmd FileType cpp,c setlocal tabstop=2 shiftwidth=2 softtabstop=2 expandtab]])
-				end
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = format_files,
+			callback = function()
+				-- Set tab width to 4 spaces for consistent formatting
+				vim.bo.tabstop = 4
+				vim.bo.shiftwidth = 4
+				vim.bo.softtabstop = 4
+				vim.bo.expandtab = true
 			end,
 		})
 	end,
